7f7879fe8e77b9e1e2d9dd5aa77e7811be1f0b32
Aditi <adudarji2002@gmail.com>
2023-08-29 21:59:10 +0530

    Add MoveForward Method

--- Chandrayan.java
+++ Chandrayan.java
@@ -1,6 +1,26 @@
 public class Chandrayan{
+    int position=0;
+    public void moveForward()
+    {
+       position++;
+    }
     public static void  main(String[] args)
     {
-
-    }
+        
+       Chandrayan sp = new Chandrayan();
+        String[] commands = {"f"};
+        for (String command : commands) {
+            if(command=="f")
+            {
+                sp.moveForward();
+            }
+            else{
+                System.out.println("Invalid Command");
+            }
+             
+        }
+   }
 }
+
+
+
