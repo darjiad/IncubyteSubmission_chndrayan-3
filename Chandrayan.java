5a38930f653a5c953502e50b6b6cd8d60b240f43
Aditi <adudarji2002@gmail.com>
2023-08-29 22:51:47 +0530

    Modify the code

--- Chandrayan.java
+++ Chandrayan.java
@@ -2,23 +2,33 @@ public class Chandrayan{
     int position=0;
     public void moveForward()
     {
-       position++;
+       position=position+1;
+    }
+    public void movebackward()
+    {
+        position=position-1;
     }
     public static void  main(String[] args)
     {
         
        Chandrayan sp = new Chandrayan();
-        String[] commands = {"f"};
-        for (String command : commands) {
-            if(command=="f")
+        String[] commands = {"f","b"};
+        for(int i=0;i<commands.length;i++)
+        {
+            if(commands[i]=="f")
             {
                 sp.moveForward();
+            }
+            else if(commands[i]=="b")
+            {
+                sp.movebackward();
             }
             else{
                 System.out.println("Invalid Command");
             }
-             
         }
+             
+        
    }
 }
 
